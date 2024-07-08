b3dcd2e28d5ccb229ad0e8a03a5df6b9fe9c25a8
JC <JQSH@users.noreply.github.com>
2024-07-08 19:05:55 +1000

    u

--- geometry/gameMechanics.js
+++ geometry/gameMechanics.js
@@ -187,7 +187,6 @@ class GameMechanics {
     render() {
         this.graphics.clear();
         this.background.draw();
-        this.graphics.drawEnvironment();
         this.graphics.drawPlayer(this.player);
         this.enemies.forEach(enemy => this.graphics.drawEnemy(enemy));
         this.bullets.forEach(bullet => this.graphics.drawBullet(bullet.x, bullet.y, bullet.angle));
